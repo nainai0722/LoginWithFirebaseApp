5af953087e20ee3713bcd71c8fc0053066c55029
tabularasa0722 <tabularasa0722@gmail.com>
2018-06-26 23:32:33 +0900

    ひとまずプロジェクト作成

---
+++ LoginWithFirebaseApp/LoginWithFirebaseApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LoginWithFirebaseApp
+//
+//  Created by apple on 2018/06/26.
+//  Copyright © 2018年 com.nainai0722. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LoginWithFirebaseApp/LoginWithFirebaseApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LoginWithFirebaseApp
+//
+//  Created by apple on 2018/06/26.
+//  Copyright © 2018年 com.nainai0722. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
